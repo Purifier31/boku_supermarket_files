a4264e4ad6b00ae6ce3514657f31dd4b6f6dd511
Purifier31 <bgreat0731@gmail.com>
2026-01-10 13:35:32 +0100

    correction
    correction

--- firebase.js
+++ firebase.js
@@ -1,6 +1,9 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+// firebase.js (ES module)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
+import { collection as fbCollection, addDoc as fbAddDoc, getDocs as fbGetDocs, query as fbQuery, orderBy as fbOrderBy } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
 
+// Replace these values with your Firebase project's config if different
 const firebaseConfig = {
   apiKey: "AIzaSyDShiu6oEezcRXolmFW0X8z2YfFjFgz3Sk",
   authDomain: "boku-supermarket.firebaseapp.com",
@@ -8,8 +11,17 @@ const firebaseConfig = {
   storageBucket: "boku-supermarket.appspot.com",
   messagingSenderId: "768497460555",
   appId: "1:768497460555:web:b4fc4d2d927bfc2ee8b4ce",
+  measurementId: "G-7BZXYDJDM7"
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const db = getFirestore(app);
 
+export { db, fbCollection, fbAddDoc, fbGetDocs, fbQuery, fbOrderBy };
+
+const documentData ={
+  "name": "Close up",
+  "code": "6151100134431",
+  "expiry": "2028-08-03",
+  "status": "valid"
+}
