aab746e3464e50dde1cf40486b6916723a259a0e
Purifier31 <bgreat0731@gmail.com>
2026-02-10 19:58:07 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,6 +3,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 import { collection as fbCollection, addDoc as fbAddDoc, getDocs as fbGetDocs, query as fbQuery, orderBy as fbOrderBy } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBzgKb9arTPpBu_uq3ELKutw3pH0NeMP7o",
   authDomain: "boku--supermarket.firebaseapp.com",
